4e32784b30ef58b13a56dc4d86488f8e5be7dd2f
Baptiste Mehat <baptiste.mehat@protonmail.com>
2024-11-08 13:14:12 +0100

    disable watson script

--- stow/tmux/.config/tmux/scripts/watson.sh
+++ stow/tmux/.config/tmux/scripts/watson.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 main() {
-
-    if  ! command -v watson > /dev/null; then
-        printf "⌛ "
-        printf "watson not installed"
-        return
-    fi
-
-
-    if [[ "$(watson status)" == "No project started." ]]; then
-        printf "⌛ "
-        return
-    fi
-
-    project=$(watson status | awk '{ print $2 }')
-    project_today=$(watson report -p ${project} -d | tail -n1 | grep  -Po "(\d+d )?(\d+h )?\d+m")
-    total_today=$(watson report -d | tail -n1 | grep  -Po "(\d+d )?(\d+h )?\d+m")
-
-
-    printf "⏳ %s %s | 📅 %s" "${project}" "${project_today}" "${total_today}"
-
+    #
+    # if  ! command -v watson > /dev/null; then
+    #     printf "⌛ "
+    #     printf "watson not installed"
+    #     return
+    # fi
+    #
+    #
+    # if [[ "$(watson status)" == "No project started." ]]; then
+    #     printf "⌛ "
+    #     return
+    # fi
+    #
+    # project=$(watson status | awk '{ print $2 }')
+    # project_today=$(watson report -p ${project} -d | tail -n1 | grep  -Po "(\d+d )?(\d+h )?\d+m")
+    # total_today=$(watson report -d | tail -n1 | grep  -Po "(\d+d )?(\d+h )?\d+m")
+    #
+    #
+    # printf "⏳ %s %s | 📅 %s" "${project}" "${project_today}" "${total_today}"
+    #
 
 }
 
