fe7917bcba4e6cf00630a4c289280c488d9d42ee
Baptiste Mehat <baptiste.mehat@protonmail.com>
2024-11-08 13:15:22 +0100

    add second vimwiki

--- stow/nvim/.config/nvim/lua/core/plugin_config/vimwiki.lua
+++ stow/nvim/.config/nvim/lua/core/plugin_config/vimwiki.lua
@@ -2,7 +2,10 @@
 vim.g.vimwiki_global_ext = 0
 
 -- location of wiki files
-vim.g.vimwiki_list = {{path = '~/vimwiki', syntax = 'markdown', ext = '.wiki'}}
+vim.g.vimwiki_list = {
+	{ path = '~/vimwiki/personal', syntax = 'markdown', ext = '.wiki' }, -- default wiki
+	{ path = '~/vimwiki/inria',    syntax = 'markdown', ext = '.wiki' } -- second wiki, accessed with '2<leader>ww'
+}
 
 -- enable to check chekboxes in wiki files
 vim.keymap.set("n", "<leader>wx", ":VimwikiToggleListItem<CR>")
