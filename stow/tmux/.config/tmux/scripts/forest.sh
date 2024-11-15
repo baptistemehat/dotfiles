02d940209b84db09e2a84e7c869f5eb56b4530ab
Baptiste Mehat <baptiste.mehat@protonmail.com>
2024-11-15 14:58:16 +0100

    update

---
+++ stow/tmux/.config/tmux/scripts/forest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+main() {
+
+    if  ! command -v forest > /dev/null; then
+        printf "⌛ "
+        printf "forest not installed"
+        return
+    fi
+
+
+    if [[ "$(forest status | awk 'NR==2')" == "No recording started." ]]; then
+        printf "⌛ "
+        return
+    fi
+
+    project=$(forest status | sed $'s/\033\[[0-9;]*m//g' | awk 'NR==2{ print $5 }')
+    # project_today=$(watson report -p ${project} -d | tail -n1 | grep  -Po "(\d+d )?(\d+h )?\d+m")
+    # total_today=$(watson report -d | tail -n1 | grep  -Po "(\d+d )?(\d+h )?\d+m")
+    total_today=$(forest report -d | tail -n1 | sed $'s/\033\[[0-9;]*m//g' | awk '{$1=$2=""; print $0}')
+
+
+    # printf "⏳ %s %s | 📅 %s" "${project}" "${project_today}" "${total_today}"
+    printf "⏳ %s %s | 📅 %s" "${project}" "${project_today}" "${total_today}"
+
+
+}
+
+main
