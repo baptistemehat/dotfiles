b65a1badcf6fb5499badf6d3ef0e4aa9baf19fc5
Baptiste Mehat <baptiste.mehat@protonmail.com>
2025-03-24 08:50:49 +0100

    update

--- stow/nvim/.config/nvim/lua/core/plugin_config/colorscheme.lua
+++ stow/nvim/.config/nvim/lua/core/plugin_config/colorscheme.lua
@@ -1,5 +1,12 @@
+vim.o.termguicolors = true
 vim.cmd 'colorscheme noctishc'
 
+vim.cmd [[
+  highlight DiffAdd    guibg=#003000 guifg=NONE
+  highlight DiffChange guibg=#303000 guifg=NONE cterm=NONE gui=NONE
+  highlight DiffDelete guibg=#300000 guifg=NONE
+  highlight DiffText   guibg=#4d3000 guifg=NONE
+]]
 
 -- Transparent nvim background
 -- vim.cmd [[
